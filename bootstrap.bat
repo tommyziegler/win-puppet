844242d589003d3d697f2f54aee8c4343f6cb6fe
Tommy Ziegler <privat@tommyz.de>
2014-06-18 20:59:08 +0200

    Update bootstrap.bat

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,8 +1,12 @@
+REM Install Chocolatey Download Manager
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
-cinst puppet
+REM Install Git
 cinst git
 
+REM Clone the current repository
 git clone https://github.com/tommyziegler/win-puppet.git
 
-call install.bat
+REM Change directory to cloned repo and call the install batch script
+cd win-puppet
+call ./install.bat
